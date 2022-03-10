7afaf80c52a8343c20d587229174d3cbd1ed9cce
Daniel Chapdelaine <orvist@gmail.com>
2022-03-10 09:45:55 -0600

    is mobile user

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component, HostBinding, ViewEncapsulation } from '@angular/core';
+import { Component, HostBinding, OnInit, ViewEncapsulation } from '@angular/core';
+// import {
+//   BreakpointObserver,
+//   BreakpointState
+// } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-root',
@@ -6,10 +10,34 @@ import { Component, HostBinding, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./app.component.css'],
   encapsulation: ViewEncapsulation.None
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   
   @HostBinding('class') classes = 'app-container';
 
+  isMobileUser = false;
   year = new Date().getFullYear()
+
+  constructor() {}
+
+  ngOnInit() {
+    const ua = navigator.userAgent;
+    if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini|Mobile|mobile|CriOS/i.test(ua)) {
+      this.isMobileUser = true;
+    } else {
+      this.isMobileUser = false;
+    }
+    console.log(this.isMobileUser);
+    
+    // console.log()
+    // this.breakpointObserver
+    //   .observe(['(min-width: 500px)'])
+    //   .subscribe((state: BreakpointState) => {
+    //     if (state.matches) {
+    //       console.log('Viewport width is 500px or greater!');
+    //     } else {
+    //       console.log('Viewport width is less than 500px!');
+    //     }
+    //   });
+  }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {MatMenuModule} from '@angular/material/menu';
+import { LayoutModule } from '@angular/cdk/layout';
+
 
 import { AppComponent } from './app.component';
 
@@ -17,6 +19,7 @@ import { AppComponent } from './app.component';
     MatButtonModule,
     MatIconModule,
     MatMenuModule,
+    LayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
